829e30155b0536dbd52112721495e8107aceea5d
lagunerio <lagunerio@gmail.com>
2017-04-27 10:58:11 +0900

    first commit and push

---
+++ ooplab7_practice/src/ooplab7_practice/test1.java
@@ -0,0 +1,11 @@
+package ooplab7_practice;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World");
+	}
+
+}
